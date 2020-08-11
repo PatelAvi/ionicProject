4cd1d509f444a036f69b14e6062657ff8bfef060
Avi Patel <avipatel1199@gmail.com>
2020-08-11 14:04:19 +0530

    add delete recipes-item component router navigate

---
+++ src/app/recipes/recipes-details/recipes-details.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RecipesService } from '../recipes.service';
+import { recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-details',
+  templateUrl: './recipes-details.page.html',
+  styleUrls: ['./recipes-details.page.scss'],
+})
+export class RecipesDetailsPage implements OnInit {
+
+  loadedRecipe : recipe;
+  constructor(private activateRoute: ActivatedRoute, private recipesservice : RecipesService,private router: Router) { }
+
+  ngOnInit() {
+    this.activateRoute.paramMap.subscribe(paramMap =>{
+      if(!paramMap.has('recipeId')){
+        return;
+      }
+      const recipeId = paramMap.get('recipeId');
+      this.loadedRecipe = this.recipesservice.getRecipe(recipeId);
+
+      if(!this.loadedRecipe.id){
+        this.router.navigate(['recipes']);
+      }
+    })
+  }
+
+  onDeleteClick(){
+    this.recipesservice.deleteRecipe(this.loadedRecipe.id);
+    this.router.navigate(['recipes']);
+  }
+
+}
